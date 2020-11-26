f011907c38417af54cc08451755afa52522c369f
Jimmy <sauronlemaudit@gmail.com>
2020-11-26 15:59:26 +0100

    template fonctionnel

--- app/src/main/java/com/example/ordergiver/MainActivity.java
+++ app/src/main/java/com/example/ordergiver/MainActivity.java
@@ -5,51 +5,25 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.view.View;
-
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.ordergiver.fragments.HomeTab;
 import com.example.ordergiver.fragments.OrderTab;
 import com.example.ordergiver.java.Adapter;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewVisible;
     private PagerAdapter pageAdapter;
-    private Button speechRecognizerButton;
-    private TextView speechRecognizerText;
-    private SpeechRecognizer speechRecognizer;
-    private Intent speechRecognizerIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setters();
-        addListeners();
-    }
-
-    public void setters()
-    {
-        this.speechRecognizerButton = findViewById(R.id.btn_mic);
-        this.speechRecognizerText = findViewById(R.id.txt_state);
-        this.speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        this.speechRecognizerIntent = new Intent(RecognizerIntent. ACTION_RECOGNIZE_SPEECH );
-        this.speechRecognizerIntent.putExtra(RecognizerIntent. EXTRA_LANGUAGE_MODEL , RecognizerIntent. LANGUAGE_MODEL_FREE_FORM );
-        this.speechRecognizerIntent.putExtra(RecognizerIntent. EXTRA_LANGUAGE , Locale.getDefault ());
+        this.initView();
     }
 
     // View configuration ----------------------
@@ -64,88 +38,6 @@ public class MainActivity extends AppCompatActivity {
         this.viewVisible = findViewById(R.id.view_visible);
         this.pageAdapter = new Adapter(getSupportFragmentManager(), fragmentList);
         this.viewVisible.setAdapter(this.pageAdapter);
-    }
-
-    public void addListeners()
-    {
-        this.speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-                setSpeechRecognizerText("Écoute ...");
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-                setSpeechRecognizerText("Touchez pour donner un ordre");
-            }
-
-            @Override
-            public void onError(int i) {
-
-            }
-
-            @Override
-            public void onResults(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-
-            }
-        });
-
-        this.getSpeechRecognizerButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSpeechRecognizer().startListening(getSpeechRecognizerIntent());
-            }
-        });
-    }
-
-    public void setSpeechRecognizerButton(Button speechRecognizerButton) {
-        this.speechRecognizerButton = speechRecognizerButton;
-    }
-
-    public Button getSpeechRecognizerButton() {
-        return this.speechRecognizerButton;
-    }
-
-    public void setSpeechRecognizerText(String speechRecognizerText) {
-        this.speechRecognizerText.setText(speechRecognizerText);
-    }
-
-    public TextView getSpeechRecognizerText() {
-        return this.speechRecognizerText;
-    }
-
-    public SpeechRecognizer getSpeechRecognizer() {
-        return speechRecognizer;
-    }
-
-    public Intent getSpeechRecognizerIntent()
-    {
-        return this.speechRecognizerIntent;
     }
 
 }
--- app/src/main/java/com/example/ordergiver/fragments/HomeTab.java
+++ app/src/main/java/com/example/ordergiver/fragments/HomeTab.java
@@ -1,17 +1,31 @@
 package com.example.ordergiver.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ordergiver.R;
 
+import java.util.Locale;
+
 public class HomeTab extends Fragment
 {
+
+    private Button speechRecognizerButton;
+    private TextView speechRecognizerText;
+    private SpeechRecognizer speechRecognizer;
+    private Intent speechRecognizerIntent;
+
     public HomeTab()
     {
         // Required empty public constructor
@@ -21,7 +35,101 @@ public class HomeTab extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         ViewGroup rotationView = (ViewGroup) inflater.inflate(R.layout.home_tab, container, false);
-
+        setters(rotationView);
+        addListeners();
         return rotationView;
     }
+
+    public void setters(ViewGroup rotationView)
+    {
+        this.speechRecognizerButton = rotationView.findViewById(R.id.btn_mic);
+        this.speechRecognizerText = rotationView.findViewById(R.id.txt_state);
+        this.speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getContext());
+        this.speechRecognizerIntent = new Intent(RecognizerIntent. ACTION_RECOGNIZE_SPEECH );
+        this.speechRecognizerIntent.putExtra(RecognizerIntent. EXTRA_LANGUAGE_MODEL , RecognizerIntent. LANGUAGE_MODEL_FREE_FORM );
+        this.speechRecognizerIntent.putExtra(RecognizerIntent. EXTRA_LANGUAGE , Locale.getDefault ());
+    }
+
+    public void addListeners()
+    {
+        this.getSpeechRecognizerButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSpeechRecognizer().startListening(getSpeechRecognizerIntent());
+            }
+        });
+
+        this.speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                setSpeechRecognizerText("Écoute ...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                setSpeechRecognizerText("Touchez pour donner un ordre");
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+    }
+
+    public void setSpeechRecognizerButton(Button speechRecognizerButton) {
+        this.speechRecognizerButton = speechRecognizerButton;
+    }
+
+    public Button getSpeechRecognizerButton() {
+        return this.speechRecognizerButton;
+    }
+
+    public void setSpeechRecognizerText(String speechRecognizerText) {
+        this.speechRecognizerText.setText(speechRecognizerText);
+    }
+
+    public TextView getSpeechRecognizerText() {
+        return this.speechRecognizerText;
+    }
+
+    public SpeechRecognizer getSpeechRecognizer() {
+        return speechRecognizer;
+    }
+
+    public Intent getSpeechRecognizerIntent()
+    {
+        return this.speechRecognizerIntent;
+    }
+
 }
